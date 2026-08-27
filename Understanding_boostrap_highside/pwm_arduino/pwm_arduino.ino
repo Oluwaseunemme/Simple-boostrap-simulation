87bb47c21b277806e099426c01ec597b93a84e9c
Oluwaseunemme <149331785+Oluwaseunemme@users.noreply.github.com>
2026-08-27 11:17:12 +0100

    Add main files upload

---
+++ Understanding_boostrap_highside/pwm_arduino/pwm_arduino.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+}
+
+void loop() {
+  analogWrite(9, 130);
+  analogWrite(6, 130);
+  // put your main code here, to run repeatedly:
+
+}
